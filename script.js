38aa90eadf7907db38f6f2ee7b6b7b2be8959c70
Carlos Arro <carlosarro182@gmail.com>
2023-01-17 11:10:49 +0400

    Added routes and data
    Added the routes and two arrays of objects, lessons and user

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const { response } = require("express");
 const express = require("express");
 const http = require('http');
 const app = express();
 
+lessons = [
+    { topic: "math", location: "Hendon", price : 100},
+    { topic: "math", location: "Colindal", price : 80},
+    { topic: "math", location: "Brent Cross", price : 90},
+    { topic: "math", location: "Golders Green", price : 120}
+
+]
+user = {email: "user@email.com", password: "mypassword"}
+
+
+app.get("/lessons",function(request,response){
+    response.send(lessons);
+})
+
+app.get("/users",function(request,response){
+    response.send(user)
+})
+
 app.use(function(request,response){
-    console.log("In comes a request to: " + request.url);
-    response.end("Hello World");
+    response.status(404).send("Page not found!");
 });
-http.createServer(app).listen(3000);
+
+
+app.listen(3000);
