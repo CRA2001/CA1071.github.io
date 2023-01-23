38ff38909a4e1d5f75e0d30549c1f7ed568b68a0
aqhafis <hafis.firosh@rakbank.ae>
2023-01-23 19:21:47 +0400

    e

--- script.js
+++ script.js
@@ -1,45 +1,25 @@
-const { response } = require("express");
-const express = require("express");
-const http = require('http');
-const { stringify } = require("querystring");
-const app = express();
-const port = 3000;
+var express = require("express");
+var app = express();
 
-lessons = [
-    { topic: "math", location: "Hendon", price : 100},
-    { topic: "math", location: "Colindal", price : 80},
-    { topic: "math", location: "Brent Cross", price : 90},
-    { topic: "math", location: "Golders Green", price : 120}
-
-]
-user = {email: "user@email.com", password: "mypassword"}
+// Cross-Origin Resource Sharing
+const cors = require("cors");
+app.use(cors());
 
 
-app.get("/lessons",function(request,res){
-    res.end(JSON.stringify(lessons));
-})
-
-app.get("/users",function(request,res){
-    res.end(JSON.stringify(user));
-})
-
-app.use(function(request,response){
-    response.status(404).send("Page not found!");
-});
-app.listen(port);
-
-// http.createServer((req,res)=>{
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     res.writeHead(200,{'Control-Type':'text/plain'});
-//     res.end(JSON.stringify(lessons));  
-// }).listen(port,()=>{
-//     console.log('Server running at http://localhost:$(port)');
-// });
-
-// app.get("/users",function(req,res){
-//     res.end(JSON,stringify(user));
-// });
+app.get("/lessons", function(request, response) {
+    response.json( [{topic: 'Math',location:'Hendon',price:100},
+                    {topic: 'Math',location:'Colindale',price:80},
+                    {topic: 'Math',location:'Brent Cross',price:90},
+                    {topic: 'Math',location:'Golden Green',price:120}]);
+    });
 
 
+app.get("/user", function(request, response) 
+    {response.json({email:"user@gmail.com",password:"mypassword"});});
 
+
+app.use(function(request, response) {response.status(404).send("Page not found!");});
+
+
+app.listen(3000);
 
